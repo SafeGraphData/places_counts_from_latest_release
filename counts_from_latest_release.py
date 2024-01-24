cab9b51297c22b249ef64c0291d9b40c86e77b10
ilanrich <54144308+ilanrich@users.noreply.github.com>
2024-01-24 17:25:57 -0500

    update markdown

--- counts_from_latest_release.py
+++ counts_from_latest_release.py
@@ -58,7 +58,7 @@ parking_lots_df_styled = (
         "Total POI": "{:,.0f}"
     })
 )
-st.markdown(f"### Total POI count across countries, including parking lots POI is <b>{total_poi:,.0f}</b>")
+st.markdown(f"### Total POI count across countries, including parking lots POI is <b>{total_poi:,.0f}</b>", unsafe_allow_html=True)
 #st.write(, unsafe_allow_html=True)
 st.dataframe(latest_release_df_styled, use_container_width=True,hide_index=True)
 st.write("Latest Release - Parking")
